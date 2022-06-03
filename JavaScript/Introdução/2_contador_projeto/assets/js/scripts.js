92ae1a10b46e72da000629857e5bd4f1e6606d37
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-06-03 13:03:44 -0300

    Projeto contador com JavaScript

--- JavaScript/Introdução/2_contador_projeto/assets/js/scripts.js
+++ JavaScript/Introdução/2_contador_projeto/assets/js/scripts.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function testNumber(){
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
+    else{
+        currentNumberWrapper.style.color = 'gray';
+    }
+}
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    testNumber();
+}
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    testNumber();
+}
