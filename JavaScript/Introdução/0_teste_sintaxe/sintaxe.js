e06d7e1fd3b7ab87036e9cd76fd3e220dffb2e36
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-06-03 12:13:16 -0300

    Aulas iniciais sobre JavaScript

---
+++ JavaScript/Introdução/0_teste_sintaxe/sintaxe.js
@@ -0,0 +1,18 @@
+//var y = 0;
+/*var x = 1;
+a
+a
+*/
+/*Comentarios igual em C++ | C# */
+
+//variavel
+var a = 1; // variavel padrão
+const b = 2; // constante
+
+//Funções
+function soma(a, b){
+    return a + b;
+}
+soma(3, 5);
+console.log(soma(3, 5));
+
