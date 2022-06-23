d3b74c803cd5ab1b141a9fecf5f676480355c335
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-06-23 12:36:23 -0300

    Desafio to-do list

---
+++ JavaScript/Introdução/3_to-do-list_projeto/assets/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('add-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+
+form.onsubmit = function (e) {
+	e.preventDefault();
+	const inputField = document.getElementById('task-input');
+	addTask(inputField.value);
+	form.reset();
+};
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('item-container');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
+}
