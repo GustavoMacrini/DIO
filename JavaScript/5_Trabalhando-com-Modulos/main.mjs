6185ec5f8cc17ce588884b013de600eeb17d26f7
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-06-28 11:03:04 -0300

    Basico de modulos - export e import

---
+++ JavaScript/5_Trabalhando-com-Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gustavo", 19));
+console.log(mostraCidade("Gustavo", "Ribeirão Preto"));
+console.log(mostraHobby("Gustavo", "Jogar"));
